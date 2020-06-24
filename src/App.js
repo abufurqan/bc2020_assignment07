54f10ddaf27602a897c5ade5fa3a8e8039ce826c
Fiaz Ahmed <fayyazahmada@gmail.com>
2020-06-24 23:46:31 +0300

    changes done at both

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, Date } from 'react';
 import './App.css';
 
 function App() {
@@ -10,6 +10,14 @@ function App() {
     async function getRepos() {
       const response = await fetch("https://api.github.com/users/abufurqan/repos")
       const data = await response.json();
+
+      // Get the Date object
+      //const date = Date(data.date);
+      // Outputs as "Fri Feb 17 2017 01:00:00 GMT+0100"
+      //const dateString = Date(data.date).toString();
+      // Outputs as "February 17, 2017"
+      //const formattedDate = Moment(date).format("LL");
+
       console.log(data);
       setRepos(data)
     }
@@ -26,13 +34,24 @@ function App() {
 
   return (
     <div className="App">
-      <h2>You are seeing all of my repositories <br />
-        check it a github by clicking my name <a href="https://github.com/abufurqan?tab=repositories" target='_blank' rel='noopener referrer'>Fiaz Ahmed</a></h2>
+      <h3>You are seeing all of my repositories, you can check it a github by clicking my name <a href="https://github.com/abufurqan?tab=repositories">Fiaz Ahmed</a></h3>
       <h4>or click any of the below repository to open it in a new tab</h4>
     
       <ol>
         {repos.map((repoObj, ind) => {
-          return (<a href={repoObj.svn_url} target='_blank' rel='noopener referrer'><li key={ind}>{repoObj.name} Creation Date: {repoObj.created_at}</li></a>)
+          return (
+            <li key={ind}>
+              <a href={repoObj.svn_url}>{repoObj.name}</a><br />
+              Language: {repoObj.language}
+            </li>
+            // date added
+            /*
+            <li key={ind}>
+              <a href={repoObj.svn_url}>{repoObj.name}</a><br />
+              Creation Date: {repoObj.created_at} Language: {repoObj.language} Updated_at: {repoObj.updated_at}
+            </li>
+            */
+          )
         })}
       </ol>
     </div>
